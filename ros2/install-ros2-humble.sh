7dad69dfbc66f756eb553b15a4e48cff68a2d614
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2023-08-03 15:12:37 -0300

    Limit colcon workers to avoid triggering OOM

--- ros2/install-ros2-humble.sh
+++ ros2/install-ros2-humble.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Recommended for a machine with 16 GB of RAM. Trying to go above 8 workers will
+# require more RAM if you do not wish to suffer severe slowdowns due to the
+# trashing or to be surprised with the consequences of triggering the OOM
+# killer.
+# Colcon doesn't really know what should be parallelised, so you will still get
+# ${WORKERS} workers fighting to use all the available cores at once. This
+# causes unnecessary context switches and plenty of cache misses.
+# Going lower or configuring the sequential executor is not recommended, as
+# colcon will refuse to parallelise the lengthy single threaded installation
+# phases, which will waste time on a machine with a SSD or fast HDDs.
+# If you somehow forget to set this variable, the script will try to use the
+# default value detected by colcon, the number of available processors. This
+# includes the SMT threads, if they are available.
+readonly WORKERS=8
+
 if command -v locale > /dev/null; then
 	if grep --quiet 'UTF-8' <<< "$(locale)"; then
 		echo "UTF-8 locale found"
@@ -26,7 +41,7 @@ sudo add-apt-repository --yes universe
 # THIS WILL OVERWRITE ROS-RELATED SYSTEM PACKAGES!
 # as root
 sudo wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O /usr/share/keyrings/ros-archive-keyring.gpg
-# Pay close attention!
+# Using sudo requires care when writing commands with shell redirections
 # echo "data" > special-file
 # echo "data" | sudo tee special-file > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/ros-archive-keyring.gpg arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
@@ -56,10 +71,13 @@ chmod +x ros2-humble-packages.sh
 sudo ./ros2-humble-packages.sh
 
 # Build the code
-# Colcon needs pty devices or it will die before compiling anything
-# If you're building in a chrooted environment, you must bind mount the required devices
+# Colcon needs pty devices or it will die before compiling anything.
+# If you're building in a chrooted environment, you must bind mount the required
+# devices.
 # as other user
+# Fixing permissions is not needed if you're compiling as someone other than the
+# super-user.
 # rosdep fix-permissions
 rosdep update
 rosdep check --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
-colcon build
+colcon build --parallel-workers "${WORKERS:=$(nproc)}"
