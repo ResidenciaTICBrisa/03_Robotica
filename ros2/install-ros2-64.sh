be9dc56adddb84102fa1cfc7a29d4fc7877a3afa
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2023-08-07 16:55:21 -0300

    Replace repeated strings with a variable
    Modify the installation scripts to remove error-prone repeated strings.

--- ros2/install-ros2-64.sh
+++ ros2/install-ros2-64.sh
@@ -2,6 +2,8 @@
 
 source './chroot-env-vars.sh'
 
-cp install-ros2-humble.sh "${ARM64_CHROOT}/home/${CHROOTED_USER}"
-chmod 755 "${ARM64_CHROOT}/home/${CHROOTED_USER}/install-ros2-humble.sh"
-run_on_chroot "${ARM64_CHROOT}" '1000' bash -c "cd /home/${CHROOTED_USER} && USER=${CHROOTED_USER} HOME=/home/${CHROOTED_USER} ./install-ros2-humble.sh"
+readonly INSTALLATION_SCRIPT='install-ros2-humble.sh'
+
+cp "${INSTALLATION_SCRIPT}" "${ARM64_CHROOT}/home/${CHROOTED_USER}"
+chmod 755 "${ARM64_CHROOT}/home/${CHROOTED_USER}/${INSTALLATION_SCRIPT}"
+run_on_chroot "${ARM64_CHROOT}" '1000' bash -c "cd /home/${CHROOTED_USER} && USER=${CHROOTED_USER} HOME=/home/${CHROOTED_USER} ./${INSTALLATION_SCRIPT}"
